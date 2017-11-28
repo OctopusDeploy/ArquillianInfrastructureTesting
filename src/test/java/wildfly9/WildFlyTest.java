11796a8452ddf0f1dbe9881e2d2fce0144110a1d
Matthew Casperson <matthewcasperson@gmail.com>
2017-11-28 16:46:56 +1000

    Started work on infrastructure testing project

---
+++ src/test/java/wildfly9/WildFlyTest.java
@@ -0,0 +1,14 @@
+package wildfly9;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class WildFlyTest {
+
+    @Test
+    public void connectToWildFly() {
+
+    }
+}
